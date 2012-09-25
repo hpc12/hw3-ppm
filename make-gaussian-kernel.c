df3e16e458cfdf7112cd757a01a877d643356e1d
Andreas Kloeckner <inform@tiker.net>
2012-09-25 16:29:13 -0400

    Output Gaussian normalization.

--- make-gaussian-kernel.c
+++ make-gaussian-kernel.c
@@ -10,17 +10,20 @@ int main()
     return EXIT_FAILURE;
   double w = img->width;
   double h = img->height;
+  int sum = 0;
   for (size_t j = 0; j<img->height; ++j)
     for (size_t i = 0; i<img->width; ++i)
     {
       double x = 4*(i-0.5*w)/w;
       double y = 4*(j-0.5*h)/h;
       x += y;
-      double val = exp(-x*x-y*y);
-      img->red_buffer[i+img->width*j] = 255*val;
-      img->green_buffer[i+img->width*j] = 255*val;
-      img->blue_buffer[i+img->width*j] = 255*val;
+      double val = 255*exp(-x*x-y*y);
+      img->red_buffer[i+img->width*j] = val;
+      img->green_buffer[i+img->width*j] = val;
+      img->blue_buffer[i+img->width*j] = val;
+      sum += (int) val;
     }
+  printf("kernel sum: %d\n", sum);
   write_ppm("gaussian-kernel.ppm", img);
   return EXIT_SUCCESS;
 }
